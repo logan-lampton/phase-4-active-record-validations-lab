0f12b2bf6149e189697181e037d99a5342cb4a3d
Logan Lampton <lampton.logan@gmail.com>
2022-10-13 19:51:09 -0400

    commit tree 1bcfd26b790f680251e8ada997910b0015e13cf0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+        message: "is not included in the list" }
+
+    validate :title_clickbaity
+
+    CLICKBAIT_TITLES = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+    
+    def title_clickbaity
+        if CLICKBAIT_TITLES.none? { |t| t.match title }
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
